b4a6fd47c804b9c5aa8f051bf47e99b006051b90
Esko <hamburjare@hotmail.com>
2023-03-12 14:58:44 +0200

    mongodb connection test

---
+++ MongoDBTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Driver;
+using dotenv.net;
+
+namespace MongoDBTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DotEnv.Load();
+            var mongodbUrl = Environment.GetEnvironmentVariable("MONGODB_URL");
+
+            var client = new MongoClient(mongodbUrl);
+
+            var dbList = client.ListDatabases().ToList();
+
+            Console.WriteLine("Databases:");
+            foreach (var db in dbList)
+            {
+                Console.WriteLine(db);
+            }
+        }
+
+    }
+}
